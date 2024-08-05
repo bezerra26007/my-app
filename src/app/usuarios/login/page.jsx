1b8d726337cdb0271a90668903f6212c2d4f2634
Gabriel Bezerra De Sousa <gabriel.sousa234@etec.sp.gov.br>
2024-08-05 20:38:33 -0300

    05/08

--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Login() {
   return (
     <div className='container'>
       <div className={styles.container_titulo}>
-        <h1 className={styles.txt_titulo}>Aula Next</h1>
+        <h1 className={styles.txt_titulo}>Login</h1>
       </div>
     </div>
 
